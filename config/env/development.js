62543bd433532013acc5c3ea1b21d921be56c52d
jlubow <joshlubow@gmail.com>
2017-10-05 21:32:53 -0400

    Extra base files  Please enter the commit message for your changes. Lines starting

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://admin:hello@ds161574.mlab.com:61574/shandsresearchdatabase',
     options: {},
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
