7e258015d8ab64ba4afe000fc3b99aceaefe00b9
nigeledavis@ufl.edu <nigeledavis@github.com>
2017-10-26 14:43:21 -0400

    made changes to filter page

--- modules/core/client/factories/studies.client.factory.js
+++ modules/core/client/factories/studies.client.factory.js
@@ -1,8 +1,8 @@
-angular.module('core').factory('Studies', ['$http', 
+angular.module('core').factory('Studies', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('https://localhost:3000/list');
+        return $http.get('http://localhost:3000/list');
       }
     };
 
