eb616c7c050e7a370abcf932007704c500d45a19
jlubow <joshlubow@gmail.com>
2017-11-05 21:56:31 -0500

    N/A

--- modules/core/client/factories/requirements.client.factory.js
+++ modules/core/client/factories/requirements.client.factory.js
@@ -2,7 +2,7 @@ angular.module('core').factory('Requirements', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('https://localhost:3000/api/requirements');
+        return $http.get('http://localhost:3000/api/requirements');
       }
     };
 
