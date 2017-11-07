2d21ee85023a95e2d9bb4d93ecaf587c0dee831d
sunnyp48 <patelsunny15@gmail.com>
2017-11-07 11:23:36 -0500

    Added Filter

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -10,29 +10,43 @@
     var vm = this;
 
     $scope.loading = true;
-  
+    
 
     $scope.find = function() {
+
     	Studies.getAll().then(function(response) {
-        $scope.loading = false; //remove loader
-        $scope.studies = response.data;
-    }, function(error) {
+            $scope.loading = false; //remove loader
+            $scope.studies = response.data;
+            }, function(error) {
         $scope.loading = false;
         $scope.error = 'Unable to retrieve studies!\n' + error;
     });
   	}
 
     $scope.showDetails = function(index) {
-    $scope.add = $scope.studies[index];
-    console.log("add: " + $scope.add.hemorrhage);
+        $scope.add = $scope.studies[index];
+    };
+
+  //***********
+      $scope.ageFilter = function(val){
+        console.log("here");
+        return (val.age_lower_bound < 18);
+    }
+    
+    
+
+  //***********
+
 
   }
 
 
-
-
-  }
 }());
+
+
+
+
+
 
 
 
