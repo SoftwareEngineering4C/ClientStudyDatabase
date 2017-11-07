b14920f7aac57d563304ed56fa0a0a917724048c
jlubow <joshlubow@gmail.com>
2017-11-07 10:48:46 -0500

    Update studies.client.factory.js

--- modules/core/client/factories/studies.client.factory.js
+++ modules/core/client/factories/studies.client.factory.js
@@ -2,15 +2,15 @@ angular.module('core').factory('Studies', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:3000/list');
+        return $http.get('https://softwareproject.herokuapp.com/list');
       },
 
       create: function(study) {
-        return $http.post('http://localhost:3000/api/studies', study);
+        return $http.post('https://softwareproject.herokuapp.com/api/studies', study);
       },
 
       delete: function(id) {
-        return $http.delete('http://localhost:3000/api/studies/' + id);
+        return $http.delete('https://softwareproject.herokuapp.com/api/studies/' + id);
       }
     };
 
