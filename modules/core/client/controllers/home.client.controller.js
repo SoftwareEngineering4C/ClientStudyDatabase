e6d809662bc9c38c6c2779811e4a8f88f362a249
sunnyp48 <patelsunny15@gmail.com>
2017-11-07 11:31:04 -0500

    Added filter

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -10,44 +10,20 @@
     var vm = this;
 
     $scope.loading = true;
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 535adea7ed35b34fbfc00c910ba7401427184429
 
     $scope.find = function() {
 
     	Studies.getAll().then(function(response) {
-<<<<<<< HEAD
             $scope.loading = false; //remove loader
             $scope.studies = response.data;
             }, function(error) {
-=======
-        $scope.loading = false; //remove loader
-        $scope.studies = response.data;
-      }, function(error) {
->>>>>>> 535adea7ed35b34fbfc00c910ba7401427184429
         $scope.loading = false;
         $scope.error = 'Unable to retrieve studies!\n' + error;
       });
   	}
 
     $scope.showDetails = function(index) {
-<<<<<<< HEAD
-        $scope.add = $scope.studies[index];
-    };
-
-  //***********
-      $scope.ageFilter = function(val){
-        console.log("here");
-        return (val.age_lower_bound < 18);
-    }
-    
-    
-
-  //***********
-=======
       $scope.add = $scope.studies[index];
     }
 
@@ -55,24 +31,11 @@
       var id = study._id;
 
       Studies.delete(id);
-      
+
     }
->>>>>>> 535adea7ed35b34fbfc00c910ba7401427184429
 
 
   }
 
 
 }());
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-=======
->>>>>>> 535adea7ed35b34fbfc00c910ba7401427184429
