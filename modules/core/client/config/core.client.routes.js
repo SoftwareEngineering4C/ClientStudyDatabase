1ac1e2c470e0a3f6f80b6bb1653a61b2ebbdfab4
Juan Parra <31254711+juanfparra12@users.noreply.github.com>
2017-11-19 11:26:12 -0500

    Update core.client.routes.js

--- modules/core/client/config/core.client.routes.js
+++ modules/core/client/config/core.client.routes.js
@@ -30,7 +30,7 @@
       .state('home', {
         url: '/',
         templateUrl: '/modules/core/client/views/home.client.view.html',
-        controller: 'HomeController',
+        controller: 'AuthenticationController',
         controllerAs: 'vm'
       })
       .state('user', {
