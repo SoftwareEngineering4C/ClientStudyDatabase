4551d49e3bc2634de9ca26877a0881f99799dc1c
sunnyp48 <patelsunny15@gmail.com>
2017-11-26 19:43:28 -0500

    edit feature

---
+++ modules/core/client/controllers/editStudy.client.controller.js
@@ -0,0 +1,21 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('core')
+    .controller('EditController', EditController);
+    //HomeController.$inject =  ['ngAnimate', 'ngSanitize', 'ui.bootstrap'];
+
+  function EditController($scope, $window, Studies, $stateParams) {
+    var vm = this;
+
+    
+    $scope.init = function(){
+      $scope.study = $stateParams.study;    
+    }
+
+
+  }
+
+
+}());
