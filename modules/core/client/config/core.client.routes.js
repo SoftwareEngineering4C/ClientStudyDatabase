2646c4a9f0d2b99759c9470fc6bb8b4de01cd999
Juan Parra <juanfparra12@ufl.edu>
2017-11-27 19:59:53 -0500

    Fixing up app

--- modules/core/client/config/core.client.routes.js
+++ modules/core/client/config/core.client.routes.js
@@ -59,6 +59,10 @@
         url: '/addRequirement',
         templateUrl: '/modules/core/client/views/addRequirement.client.view.html'
       })
+      .state('newHome', {
+        url: '/newHome',
+        templateUrl: '/modules/core/client/views/newHome.client.view.html'
+      })
       .state('not-found', {
         url: '/not-found',
         templateUrl: '/modules/core/client/views/404.client.view.html',
