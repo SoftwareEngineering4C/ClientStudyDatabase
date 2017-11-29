791538c20dbdf50218add8f8266c9229d732a86e
jlubow <joshlubow@gmail.com>
2017-11-29 13:04:17 -0500

    Changed factories to work with localhost

--- modules/core/client/factories/studies.client.factory.js
+++ modules/core/client/factories/studies.client.factory.js
@@ -2,19 +2,19 @@ angular.module('core').factory('Studies', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('https://softwareproject.herokuapp.com/api/studies');
+        return $http.get('http://localhost:3000/api/studies');
       },
 
       create: function(study) {
-        return $http.post('https://softwareproject.herokuapp.com/api/studies', study);
+        return $http.post('http://localhost:3000/api/studies', study);
       },
 
       delete: function(id) {
-        return $http.delete('https://softwareproject.herokuapp.com/api/studies/' + id);
+        return $http.delete('http://localhost:3000/api/studies/' + id);
       },
 
       update: function(study) {
-        return $http.put('https://softwareproject.herokuapp.com/api/studies', study);
+        return $http.put('http://localhost:3000/api/studies', study);
       }
     };
 
