160ac3873df168c113e06eb7f1003bed532086cb
jlubow <joshlubow@gmail.com>
2017-12-01 08:50:30 -0500

    Updated factories for heroku

--- modules/core/client/factories/requirements.client.factory.js
+++ modules/core/client/factories/requirements.client.factory.js
@@ -2,19 +2,19 @@ angular.module('core').factory('Requirements', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:3000/api/requirements/');
+        return $http.get('https://softwareproject.herokuapp.com/api/requirements/');
       },
 
       getOne: function(databaseName) {
-        return $http.get('http://localhost:3000/api/requirements/' + databaseName);
+        return $http.get('https://softwareproject.herokuapp.com/api/requirements/' + databaseName);
       },
 
       create: function(requirement) {
-        return $http.post('http://localhost:3000/api/requirements/', requirement);
+        return $http.post('https://softwareproject.herokuapp.com/api/requirements/', requirement);
       },
 
       delete: function(id) {
-        return $http.delete('http://localhost:3000/api/requirements/' + id);
+        return $http.delete('https://softwareproject.herokuapp.com/api/requirements/' + id);
       }
 
     }
